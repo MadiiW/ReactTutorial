c6a0d51631cb397c117d4a7f12189e99fa3372be
mw229 <mw229@hdm-stuttgart.de>
2025-04-26 11:52:21 +0200

    added todo to exercise 2

--- exercises/exercise02/src/App.tsx
+++ exercises/exercise02/src/App.tsx
@@ -2,6 +2,7 @@ import ChartsPage from "./pages/ChartsPage";
 
 export default function App() {
   return (
+    // TODO 1: Nutze den AudioProvider
     <div className="min-h-screen bg-[#222163] text-white p-8">
       <ChartsPage />
     </div>
--- exercises/exercise02/src/pages/ChartsPage.tsx
+++ exercises/exercise02/src/pages/ChartsPage.tsx
@@ -5,22 +5,23 @@ export default function ChartsPage() {
   const [topTracks, setTopTracks] = useState<Track[]>([]);
 
   useEffect(() => {
-    // TODO 2: Schreibe hier einen API Call der die aktuellen Top 10 Tracks von Deezer abruft
+    // TODO 3: Schreibe hier einen API Call der die aktuellen Top 10 Tracks von Deezer abruft
     // Die API Schnittstelle ist: /api/chart/0/tracks?limit=10
-    // Du hast alles richtig gemacht wenn du die Namen der Titel auf der Webseite angezeigt bekommst
+    // Du hast alles richtig gemacht, wenn du die Namen der Titel auf der Webseite angezeigt bekommst
     setTopTracks([]);
   }, []);
 
   return (
     <div className="mb-10">
       {
-        // TODO 1: Hier soll der Song des Tages angezeigt werden
+        // TODO 2: Hier soll der Song des Tages angezeigt werden
         // Wenn du herausfindest wie es geht, kannst du auch einen eigenen Song aussuchen der hier angezeigt wird ;)
+        // Du hast bei Aufgabe 1 + 2 alles richtig gemacht, wenn auf der Webseite ein Song angezeigt wird, den du abspielen kannst
       }
       <h1 className="text-2xl font-bold mb-4">Top 10 Charts</h1>
       <ul className="grid sm:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-4">
         {topTracks.map((track, index) => (
-          // TODO 3: Implementiere eine SongCard Component und zeige sie hier für jeden Song an
+          // TODO 4: Implementiere eine SongCard Component und zeige sie hier für jeden Song an
           // Dabei kannst du dich von der SongOfTheDay Component inspirieren lassen
           // Als Props sollst du den Track und die Zahl der Platzierung in den Charts mitgeben
           <h1>
