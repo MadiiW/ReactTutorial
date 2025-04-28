bc6de60897a8f5c41ab282bf3280c23ff649b729
mw229 <mw229@hdm-stuttgart.de>
2025-04-28 17:16:54 +0200

    added link directory

---
+++ examples/src/components/LinkDirectory.tsx
@@ -0,0 +1,34 @@
+// LinkDirectory.js
+import { Link } from "react-router-dom";
+
+export default function LinkDirectory() {
+  const routes = [
+    { path: "/", name: "Home" },
+    { path: "/clickbutton", name: "ClickButton" },
+    { path: "/clickcounter", name: "ClickCounter" },
+    { path: "/counter", name: "Counter" },
+    { path: "/auth", name: "Auth" },
+    { path: "/pokemon", name: "Pokemon" },
+    { path: "/tailwind", name: "Tailwind" },
+    { path: "/users/:id", name: "User" },
+    { path: "/dashboard", name: "Dashboard" },
+    { path: "/dashboard/profile", name: "Profile" },
+    { path: "/dashboard/settings", name: "Settings" },
+    { path: "/links", name: "Links" },
+  ];
+
+  return (
+    <div>
+      <h1 className="mb-4">Link Verzeichnis</h1>
+      <ul className="space-y-2">
+        {routes.map((route, index) => (
+          <li key={index}>
+            <Link className="text-blue-500 hover:underline" to={route.path}>
+              {route.name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
--- examples/src/pages/HomePage.tsx
+++ examples/src/pages/HomePage.tsx
@@ -1,7 +1,11 @@
+import LinkDirectory from "../components/LinkDirectory";
+
 export default function HomePage() {
   return (
     <>
       <h1>Mein erstes React-Projekt 🚀</h1>
+      <hr className="m-8" />
+      <LinkDirectory />
     </>
   );
 }
