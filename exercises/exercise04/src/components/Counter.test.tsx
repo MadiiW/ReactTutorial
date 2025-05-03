e75fd5502a818b658b402190080855954ee73375
mw229 <mw229@hdm-stuttgart.de>
2025-05-03 19:30:11 +0200

    updated todos in test

--- exercises/exercise04/src/components/Counter.test.tsx
+++ exercises/exercise04/src/components/Counter.test.tsx
@@ -1,20 +1,22 @@
-import { describe, it, expect } from 'vitest';
-import '@testing-library/jest-dom';
+import { describe, it } from "vitest";
+import "@testing-library/jest-dom";
 
 // TODO: Implementiere die Tests für die Counter-Komponente. Vergiss nicht, auch die nötigen Importe am Anfang der Datei hinzuzufügen.
-describe('Counter', () => {
-  it('zeigt den Anfangszähler', () => {
-    throw new Error('Test noch nicht implementiert');
-    // TODO: rendere die Counter-Komponente und überprüfe, ob der Text "Count: 0" im Dokument ist
+describe("Counter", () => {
+  it("zeigt den Anfangszähler", () => {
+    throw new Error("Test noch nicht implementiert");
+    // TODO 1: rendere die Counter-Komponente und überprüfe, ob der Text "Count: 0" im Dokument ist
   });
 
-  it('erhöht den Zähler bei Klick', async () => {
-    throw new Error('Test noch nicht implementiert');
-    // TODO: Rendere die Counter-Komponente, finde den Button mit Rolle "button" und Name "Increment", simuliere einen Klick darauf und überprüfe, ob "Count: 1" angezeigt wird.
+  it("erhöht den Zähler bei Klick", async () => {
+    throw new Error("Test noch nicht implementiert");
+    // TODO 2: Rendere die Counter-Komponente, finde den Button mit Rolle "button" und Name "Increment",
+    // simuliere einen Klick darauf und überprüfe, ob "Count: 1" angezeigt wird.
   });
 
-  it('zeigt eine Nachricht nach 5 Klicks', async () => {
-    throw new Error('Test noch nicht implementiert');
-    // TODO: Rendere  die Counter-Komponente, finde den Button, simuliere 5 Klicks in einer Schleife und überprüfe, ob die Nachricht "You\'ve clicked a lot!" im Dokument ist.
+  it("zeigt eine Nachricht nach 5 Klicks", async () => {
+    throw new Error("Test noch nicht implementiert");
+    // TODO 3: Rendere  die Counter-Komponente, finde den Button, simuliere 5 Klicks in einer Schleife und überprüfe,
+    // ob die Nachricht "You\'ve clicked a lot!" im Dokument ist.
   });
 });
